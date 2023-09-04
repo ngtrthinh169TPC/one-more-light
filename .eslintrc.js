c640953cd81bdf7d5d14c95e1f424f87c0f326a9
ngtrthinh <ng.tr.thinh@gmail.com>
2023-09-05 01:16:23 +0700

    chore: setup eslint for tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "@remix-run/eslint-config/node",
     "@remix-run/eslint-config/jest-testing-library",
     "prettier",
+    "plugin:tailwindcss/recommended",
   ],
   env: {
     "cypress/globals": true,
