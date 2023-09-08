d3b796e2fe36b1f56367ac7ce64155a5e22ad55b
ngtrthinh <ng.tr.thinh@gmail.com>
2023-09-08 13:49:49 +0700

    feat: no blog state

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -17,38 +17,47 @@ export default function BlogList() {
   return (
     <div className="relative min-h-screen bg-light-1-background font-primary sm:flex sm:flex-col sm:items-center">
       <Navbar />
-
-      <main className="mt-12 flex w-[600px] max-w-2xl flex-col gap-12">
-        <h1 className="font-secondary text-2xl font-bold">Hello World</h1>
-        {data.blogList.map((item) => (
-          <article key={`blog-${item.id}`}>
-            <header>
-              <h3 className="font-secondary text-xl font-bold text-light-1-primary">
-                <NavLink to={`${item.id}`}>
-                  {item.type}: {item.title}
-                </NavLink>
-              </h3>
-              <p className="text-sm" suppressHydrationWarning>
-                {new Date(item.createdAt).toLocaleDateString(undefined, {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                })}{" "}
-                • by{" "}
-                <span className="italic">
-                  {item.createdBy.displayName || item.createdBy.email}
-                </span>
-              </p>
-            </header>
-            <div
-              className="mt-3 line-clamp-3 overflow-hidden text-ellipsis whitespace-pre-line"
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(marked(item.body)),
-              }}
-            />
-          </article>
-        ))}
-      </main>
+      {data.blogList.length > 0 ? (
+        <main className="mt-12 flex w-[600px] max-w-2xl flex-col gap-12">
+          <h1 className="font-secondary text-2xl font-bold">Hello World</h1>
+          {data.blogList.map((item) => (
+            <article key={`blog-${item.id}`}>
+              <header>
+                <h3 className="font-secondary text-xl font-bold text-light-1-primary">
+                  <NavLink to={`${item.id}`}>
+                    {item.type}: {item.title}
+                  </NavLink>
+                </h3>
+                <p className="text-sm" suppressHydrationWarning>
+                  {new Date(item.createdAt).toLocaleDateString(undefined, {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}{" "}
+                  • by{" "}
+                  <span className="italic">
+                    {item.createdBy.displayName || item.createdBy.email}
+                  </span>
+                </p>
+              </header>
+              <div
+                className="mt-3 line-clamp-3 overflow-hidden text-ellipsis whitespace-pre-line"
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(marked(item.body)),
+                }}
+              />
+            </article>
+          ))}
+        </main>
+      ) : (
+        <div className="mt-24 w-[600px]">
+          There's no blog posted yet :(
+          <br />
+          If you are a guest, please come back later.
+          <br />
+          If you are the author, please fricking write something.
+        </div>
+      )}
     </div>
   );
 }
